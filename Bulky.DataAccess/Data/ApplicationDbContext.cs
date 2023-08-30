78cbe7d7f00161fda73e8dead8363c1449129279
Siddhant singh <Siddhant singh@LAPTOP-CCS20I7J>
2023-08-30 22:57:39 +0530

    Add project files.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
---
+++ BulkyWeb_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
